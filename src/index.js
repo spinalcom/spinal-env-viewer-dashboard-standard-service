b4ff6afd8188397525ea88ea8bdb2f0b4b248461
mbaradji221 <mbaradji221@outlook.fr>
2019-01-04 16:55:25 +0100

    remove dashboard linked

--- src/index.js
+++ src/index.js
@@ -14,15 +14,15 @@ const {
 
 let dashboardService = {
   createStandardDashBoardContext(contextName) {
-
     let context = SpinalGraphService.getContext(contextName);
 
     if (typeof context !== "undefined") return false;
 
-
-    return SpinalGraphService.addContext(contextName, dashboardVariables.DASHBOARD_CONTEXT_TYPE,
-      new AbstractElement(contextName));
-
+    return SpinalGraphService.addContext(
+      contextName,
+      dashboardVariables.DASHBOARD_CONTEXT_TYPE,
+      new AbstractElement(contextName)
+    );
   },
   createStandardDashBoard(contextId, dashboardName, dashboardType, attributes) {
     let abstract = new AbstractElement(dashboardName);
@@ -38,9 +38,11 @@ let dashboardService = {
     });
 
     let abstractNode = SpinalGraphService.createNode({
-      name: dashboardName,
-      type: dashboardType
-    }, abstract);
+        name: dashboardName,
+        type: dashboardType
+      },
+      abstract
+    );
 
     SpinalGraphService.addChildInContext(
       contextId,
@@ -51,116 +53,141 @@ let dashboardService = {
     );
   },
 
-  async getDashboardByType(contextId, dashboardType) {
+  getDashboardByType(contextId, dashboardType) {
+    return SpinalGraphService.getChildren(
+      contextId,
+      dashboardVariables.RELATION_NAME
+    ).then(children => {
+      let res = [];
 
-    let children = await SpinalGraphService.getChildren(contextId,
-      dashboardVariables.RELATION_NAME);
-
-
-    let res = [];
-
-    for (let i = 0; i < children.length; i++) {
-      const child = children[i];
-      if (child.type.get() === dashboardType) {
-        res.push(child);
+      for (let i = 0; i < children.length; i++) {
+        const child = children[i];
+        if (child.type.get() === dashboardType) {
+          res.push(child);
+        }
       }
 
-    }
-
-    return res;
-
+      return res;
+    });
   },
-  async hasDashBoard(nodeId) {
-    let children = await SpinalGraphService.getChildren(nodeId,
-      dashboardVariables.ENDPOINT_RELATION_NAME);
-
-
-    return children.length > 0
+  hasDashBoard(nodeId) {
+    return SpinalGraphService.getChildren(
+      nodeId,
+      dashboardVariables.ENDPOINT_RELATION_NAME
+    ).then(children => {
+      return children.length > 0;
+    });
   },
-  async linkToDashboard(nodeId, dashboardId) {
-
-
-    if (await dashboardService.hasDashBoard(nodeId)) return;
-
-
+  linkToDashboard(contextId, nodeId, dashboardId) {
+    console.log("link To dashboard");
+    // dashboardService.unLinkToDashBoard(dashboardId, nodeId, () => {
     let dashboardInfo = SpinalGraphService.getInfo(dashboardId);
 
-    let element = await dashboardInfo.element.load();
+    dashboardInfo.element.load().then(element => {
+      SpinalGraphService.addChildInContext(
+        dashboardId,
+        nodeId,
+        contextId,
+        dashboardVariables.DASHBOARD_TO_ELEMENT_RELATION,
+        SPINAL_RELATION_TYPE
+      );
 
+      let sensor = element.sensor.get();
 
-    /** Ajouter id du node dans le dasboard */
-    // if (!element.connected) {
-    //   element.add_attr({
-    //     connected: []
-    //   })
-    // }
-    // element.connected.push(nodeId);
-    /**       Fin             */
+      sensor.forEach(attr => {
+        let endpoint = new SpinalEndpoint(
+          attr.name,
+          "SpinalEndpoint",
+          attr.value,
+          attr.unit,
+          attr.dataType,
+          0,
+          30,
+          attr.dataType
+        );
 
+        let child = SpinalGraphService.createNode({
+            name: dashboardInfo.name.get(),
+            type: dashboardInfo.type.get()
+          },
+          endpoint
+        );
 
-    SpinalGraphService.addChild(dashboardId, nodeId, dashboardVariables.DASHBOARD_TO_ELEMENT_RELATION,
-      SPINAL_RELATION_TYPE);
-
-
-    /** Attribuer id dashboard à l'element nodeId */
-    // SpinalGraphService.getInfo(nodeId).element.load().then(el => {
-    //   if (!el.dashboardId) {
-    //     el.add_attr({
-    //       dashboardId: dashboardId
-    //     })
-    //   } else {
-    //     el.dashboardId.set(dashboardId);
-    //   }
-    // })
-    /** Fin */
-
-    let sensor = element.sensor.get();
-
-
-    // Pour chaque element du sensor ajouter un endpoint à la relation hasEndpoint
-
-    sensor.forEach(attr => {
-
-      let endpoint = new SpinalEndpoint(
-        attr.name,
-        "SpinalEndpoint",
-        attr.value,
-        attr.unit,
-        attr.dataType,
-        0,
-        30,
-        attr.dataType
-      )
-
-      let child = SpinalGraphService.createNode({
-        name: dashboardInfo.name.get(),
-        type: dashboardInfo.type.get()
-      }, endpoint);
-
-      SpinalGraphService.addChild(nodeId, child,
-        dashboardVariables
-        .ENDPOINT_RELATION_NAME,
-        SPINAL_RELATION_TYPE);
+        SpinalGraphService.addChild(
+          nodeId,
+          child,
+          dashboardVariables.ENDPOINT_RELATION_NAME,
+          SPINAL_RELATION_TYPE
+        );
+      });
     });
-
+    // });
   },
-  async getAllDashboardContext() {
-    let res = [];
+  getAllDashboardContext() {
     let graph = SpinalGraphService.getGraph();
 
-    let contexts = await graph.getChildren(["hasContext"]);
+    return graph.getChildren(["hasContext"]).then(contexts => {
+      let res = [];
 
-    contexts.forEach(context => {
-      if (context.info.type.get() == dashboardVariables.DASHBOARD_CONTEXT_TYPE) {
-        res.push(context.info);
+      contexts.forEach(context => {
+        if (
+          context.info.type.get() == dashboardVariables.DASHBOARD_CONTEXT_TYPE
+        ) {
+          res.push(context.info);
+        }
+      });
+
+      return res;
+    });
+  },
+  unLinkToDashBoard(dashboardId, nodeId, callback) {
+    console.log("unLinkMethod called");
+    dashboardService.hasDashBoard(nodeId).then(el => {
+      if (el) {
+        console.log("has Dashboard");
+        dashboardService.removeAllEndpoints(nodeId).then(() => {
+          console.log("now remove dashboard connected");
+          SpinalGraphService.removeChild(
+            dashboardId,
+            nodeId,
+            dashboardVariables.DASHBOARD_TO_ELEMENT_RELATION,
+            SPINAL_RELATION_TYPE
+          ).then(
+            () => {
+              console.log("call callback");
+
+              callback();
+            },
+            error => {
+              console.log("error", error);
+            }
+          );
+        });
+      } else {
+        console.log("has not dashboard");
+        callback();
       }
-    })
+    });
+  },
+  removeAllEndpoints(nodeId) {
+    console.log("remove all endpoints called");
+    return SpinalGraphService.getChildren(nodeId, [
+      dashboardVariables.ENDPOINT_RELATION_NAME
+    ]).then(endpoints => {
+      for (let i = 0; i < endpoints.length; i++) {
+        SpinalGraphService.removeChild(
+          nodeId,
+          endpoints[i].id.get(),
+          dashboardVariables.ENDPOINT_RELATION_NAME,
+          SPINAL_RELATION_TYPE
+        );
+      }
 
-    return res;
-
+      console.log("all endpoints removed");
+      return;
+    });
   }
-
-}
+};
 
 export {
   dashboardService,
